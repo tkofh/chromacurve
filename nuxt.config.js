3ff29c4c8efbea6627c5b63ef0394f4dc98c5f1d
Tim Morris <timthewebguy@gmail.com>
2021-02-24 20:31:11 -0500

    :sparkles: Meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,13 +2,12 @@ export default {
   target: 'static',
 
   head: {
-    title: 'chromacurve',
+    title: 'C 🔴 O 🟠 L 🟡 O 🟢 R 🔵 S 🟣 !',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   css: [],
@@ -27,8 +26,23 @@ export default {
   modules: ['@nuxtjs/pwa'],
 
   pwa: {
-    manifest: {
+    meta: {
       lang: 'en',
+      name: 'Color',
+      author: 'Tim Morris',
+      theme_color: '#26b7ff',
+      ogHost: 'https://color.tmo.dev',
+      ogImage: {
+        width: 1200,
+        height: 627,
+        path: 'https://color.tmo.dev/og.png',
+        mime: 'image/png',
+      },
+    },
+    manifest: {
+      name: 'Color',
+      short_name: 'Color',
+      display: 'standalone',
     },
   },
 
